b2ea02f83b391ad4542b58dc83724057fadcd260
Ellen Mota <ellenmota29@gmail.com>
2017-05-02 14:44:28 -0300

    Versao 1.0 gerador de numeros

---
+++ Gerador-numeros-randomicos/Gerador-numeros-randomicos/Gerador-numeros-randomicos/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Gerador-numeros-randomicos
+//
+//  Created by Ellen Bianca Mota de Carvalho on 02/05/17.
+//  Copyright © 2017 Ellen Bianca Mota de Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var resultado: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func gerarNumero(_ sender: Any) {
+        
+        var myRandom = arc4random_uniform(20)
+        resultado.text = String(myRandom)
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
